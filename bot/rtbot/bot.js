41290339960f1e5ccbf3a4f5ce6bcebb82c9420f
gshemdoe <georgehmariki@gmail.com>
2024-01-15 10:45:25 +0300

    final final update

--- bot/rtbot/bot.js
+++ bot/rtbot/bot.js
@@ -372,9 +372,7 @@ const rtfunction = async () => {
                     } else if (cdata == 'voda') {
                         await call_function.mtandaoCallBack(bot, ctx, chatid, imp, 17, cmsgid)
                     } else if (cdata == 'tigo') {
-                        await bot.telegram.copyMessage(chatid, imp.matangazoDB, 18)
-                        await delay(500)
-                        await call_function.mtandaoCallBack(bot, ctx, chatid, imp, 94, cmsgid)
+                        await call_function.mtandaoCallBack(bot, ctx, chatid, imp, 95, cmsgid)
                     } else if (cdata == 'airtel') {
                         await call_function.mtandaoCallBack(bot, ctx, chatid, imp, 19, cmsgid)
                     } else if (cdata == 'halotel') {
