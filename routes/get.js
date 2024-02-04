c84788c4682ad3621bda858926058738d9f69c88
gshemdoe <georgehmariki@gmail.com>
2024-02-05 00:42:35 +0300

    updated

--- routes/get.js
+++ routes/get.js
@@ -67,7 +67,7 @@ router.get('/', async (req, res) => {
 })
 
 router.get('/gsb/register', (req, res) => {
-    res.redirect('https://track.africabetpartners.com/visit/?bta=35468&nci=5439')
+    res.redirect('https://www.betway.co.tz/?btag=P94949-PR24696-CM77068-TS1971458&')
 })
 
 router.get('/pmatch/register', (req, res) => {
