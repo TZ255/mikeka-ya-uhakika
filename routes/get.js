dd18db3dc5a97a9ddd9c9a1fd29ad453797813c3
gshemdoe <georgehmariki@gmail.com>
2024-02-23 16:57:38 +0300

    updated to betway

--- routes/get.js
+++ routes/get.js
@@ -88,7 +88,9 @@ router.get('/odds-blog', async (req, res) => {
 })
 
 router.get('/gsb/register', (req, res) => {
-    res.redirect('https://track.africabetpartners.com/visit/?bta=35468&nci=5657')
+    let bway = `https://www.betway.co.tz/?btag=P94949-PR24696-CM77068-TS1971458&`
+    let gsb = `https://track.africabetpartners.com/visit/?bta=35468&nci=5657`
+    res.redirect(bway)
 })
 
 router.get('/pmatch/register', (req, res) => {
