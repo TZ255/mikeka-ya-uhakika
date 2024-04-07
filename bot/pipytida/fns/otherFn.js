96c17c95be74b594969078ec8f3fc7a194c139dd
gshemdoe <georgehmariki@gmail.com>
2024-04-07 18:56:33 +0300

    muting videos

--- bot/pipytida/fns/otherFn.js
+++ bot/pipytida/fns/otherFn.js
@@ -19,12 +19,12 @@ const verifyFn = async (bot, ctx, imp) => {
             })
             let mention = `<a href="tg://user?id=${userid}">${ver_user.fname}</a>`
             let msg = await ctx.reply(`Mtoa huduma ${mention} ameongezwa kwenye list ya watoa huduma waliothibitishwa kwenye group hili.`, { parse_mode: 'HTML' })
-            await toDeleteModel.create({msgid: msg.message_id, chatid: ctx.chat.id})
+            await toDeleteModel.create({ msgid: msg.message_id, chatid: ctx.chat.id })
         } else {
             let ver_user = await verifiedList.findOneAndUpdate({ chatid: userid }, { $set: { paid: true } }, { new: true })
             let mention = `<a href="tg://user?id=${userid}">${ver_user.fname}</a>`
             let msg = await ctx.reply(`Mtoa huduma ${mention} ameongezwa kwenye list ya watoa huduma waliothibitishwa kwenye group hili.`, { parse_mode: 'HTML' })
-            await toDeleteModel.create({msgid: msg.message_id, chatid: ctx.chat.id})
+            await toDeleteModel.create({ msgid: msg.message_id, chatid: ctx.chat.id })
         }
     } catch (error) {
         await ctx.reply(error.message)
@@ -55,6 +55,25 @@ const UnverifyFn = async (bot, ctx, imp) => {
     }
 }
 
+//mute tangazo for 5 minutes
+const muteVideosPhotos = async (bot, ctx, imp) => {
+    try {
+        let unixNow = ctx.message.date
+        let chatid = ctx.message.from.id
+        //cant send other things except the one listed below
+        await ctx.restrictChatMember(chatid, {
+            until_date: unixNow + 600,
+            permissions: {
+                can_send_messages: true,
+                can_send_voice_notes: true,
+                can_send_other_messages: true
+            }
+        })
+    } catch (error) {
+        console.log(error.message, error)
+    }
+}
+
 const checkSenderFn = async (bot, ctx, imp) => {
     try {
         let msg_id = ctx.message.message_id
@@ -81,6 +100,10 @@ const checkSenderFn = async (bot, ctx, imp) => {
             setTimeout(() => {
                 ctx.deleteMessage(msg_id).catch(e => console.log(e.message))
             }, 30000)
+        }
+        if((data && data.paid == true) && caption.length > 50) {
+            //mute 10 minutes
+            await muteVideosPhotos(bot, ctx, imp)
         }
     } catch (error) {
         console.log(error.message, error)
@@ -144,11 +167,11 @@ const watoaHuduma = async (bot, imp) => {
             txt = txt + `<b>👧 ${username} - (${w.fname})</b>\n📞 <b>Phone: ${phone}</b>\n📍 <b>Location: </b><i>${loc}</i>\n\n\n`
         }
         let msg = await bot.telegram.sendMessage(imp.r_chatting, `${txt}\n\n⚠ Kama wewe ni mtoa huduma au dalali na unataka kufanya kazi kwenye group hili, wasiliana na admin hapa <b>@Blackberry255</b> ili kuthibitishwa.\n\n<b>⚠ Tafadhali</b> Usiwasiliane na Admin kama wewe sio mtoa huduma, atakublock na nitakutoa kwenye group (hatupendi usumbufu) 😏.`, { parse_mode: 'HTML' })
-        await toDeleteModel.create({msgid: msg.message_id, chatid: msg.chat.id})
+        await toDeleteModel.create({ msgid: msg.message_id, chatid: msg.chat.id })
         setTimeout(() => {
             bot.telegram.sendMessage(imp.r_chatting, `<b>Mteja!</b> Ukikutana na mtoa huduma asiye mwaminifu ndani ya group hili, tafadhali report kwa: \n\n<b>1. Sister G (@mamyy98)</b>\nau\n<b>2. Fetty Love (@fetyy10)</b>\n\nBaada ya kureport wataondolewa kwenye group. Tusaidiane jamani kukomesha matapeli humu ndani 😁`, { parse_mode: 'HTML' })
-            .then((msg)=> {toDeleteModel.create({msgid: msg.message_id, chatid: msg.chat.id}).catch(e=> console.log(e.message))})
-            .catch(e => console.log(e.message, e))
+                .then((msg) => { toDeleteModel.create({ msgid: msg.message_id, chatid: msg.chat.id }).catch(e => console.log(e.message)) })
+                .catch(e => console.log(e.message, e))
         }, 15000)
     } catch (error) {
         console.log(error.message, error)
@@ -162,7 +185,7 @@ const updateLocation = async (bot, ctx) => {
         let txt = ctx.message.text
         let chatid = ctx.message.reply_to_message.from.id
         let loc = txt.split('loc=')[1].trim()
-        let user = await verifiedList.findOneAndUpdate({chatid}, {$set: {loc}}, {new: true})
+        let user = await verifiedList.findOneAndUpdate({ chatid }, { $set: { loc } }, { new: true })
         await ctx.reply(`Mtoa Huduma ${user.fname} location yake imeongezwa kuwa ${loc}`)
     } catch (error) {
         await ctx.reply(error.message)
@@ -175,7 +198,7 @@ const updatePhone = async (bot, ctx) => {
         let txt = ctx.message.text
         let chatid = ctx.message.reply_to_message.from.id
         let phone = txt.toLowerCase().split('phone=')[1].trim()
-        let user = await verifiedList.findOneAndUpdate({chatid}, {$set: {phone}}, {new: true})
+        let user = await verifiedList.findOneAndUpdate({ chatid }, { $set: { phone } }, { new: true })
         await ctx.reply(`Mtoa Huduma ${user.fname} namba yake ya simu imewekwa kuwa ${phone}`)
     } catch (error) {
         await ctx.reply(error.message)
@@ -199,5 +222,5 @@ const clearingGroup = async (bot, imp, delay) => {
 }
 
 module.exports = {
-    verifyFn, UnverifyFn, checkSenderFn, adminReplyToMessageFn, adminReplyTextToPhotoFn, watoaHuduma, updateLocation, updatePhone, clearingGroup
+    verifyFn, UnverifyFn, checkSenderFn, adminReplyToMessageFn, adminReplyTextToPhotoFn, watoaHuduma, updateLocation, updatePhone, clearingGroup, muteVideosPhotos
 }
