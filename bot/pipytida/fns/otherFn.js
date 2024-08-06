f47fa1550c4ae8536c2d1be844821db7ba742612
gshemdoe <georgehmariki@gmail.com>
2024-08-06 10:42:21 +0300

    updated

--- bot/pipytida/fns/otherFn.js
+++ bot/pipytida/fns/otherFn.js
@@ -143,8 +143,7 @@ const reusableRestriction = async (bot, ctx, caption, charsNum, delay) => {
                     await ctx.api.deleteMessage(ctx.chat.id, msgid)
                         .catch(e => console.log(e))
                     await ctx.reply(`Mtoa huduma ${tag} tafadhali wasiliana na admin @Blackberry255`, {parse_mode: 'HTML'})
-                    await list.updateOne({ $set: { paid: false } })
-                    await bot.api.sendMessage(1101685785, `${list.fname} paid false`) //blackberry
+                    await bot.api.sendMessage(1101685785, `${list.fname} has to pay`) //blackberry
                 }
             }
         }
